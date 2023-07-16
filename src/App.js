599d3c6a4d00e42378429c94998098d5db50a1ae
Mert Atakan Duduoğlu <atakanduduoglu@gmail.com>
2023-07-17 01:02:33 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import TodoTable from "./components/TodoTable";
+import NewTodoForm from "./components/NewTodoForm";
 
 function App() {
+  const [showAddTodoForm, setShowAddTodoForm] = useState(false);
+
+  const [todos, setTodos] = useState([
+    { rowNumber: 1, rowDescription: "Feed Dog", rowAssigned: "User One" },
+    { rowNumber: 2, rowDescription: "Water Plant", rowAssigned: "User Two" },
+    { rowNumber: 3, rowDescription: "Make Dinner", rowAssigned: "User Three" },
+    {
+      rowNumber: 4,
+      rowDescription: "Charge Phone Battery",
+      rowAssigned: "User One",
+    },
+  ]);
+
+  const addTodo = (description, assigned) => {
+    let rowNumber = 0;
+    if (todos.length > 0) {
+      rowNumber = todos[todos.length - 1].rowNumber + 1;
+    } else {
+      rowNumber = 1;
+    }
+    const newTodo = {
+      rowNumber: rowNumber,
+      rowDescription: description,
+      rowAssigned: assigned,
+    };
+    setTodos((todos) => [...todos, newTodo]);
+  };
+
+  const deleteTodos = (deleteTodoRowNumber) => {
+    let filtered = todos.filter(function (value) {
+      return value.rowNumber !== deleteTodoRowNumber;
+    });
+    setTodos(filtered);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">Your Todo's</div>
+        <div className="card-body">
+          <TodoTable todos={todos} deleteTodo={deleteTodos} />
+          <button
+            className="btn btn-primary"
+            onClick={() => setShowAddTodoForm(!showAddTodoForm)}
+          >
+            {showAddTodoForm ? 'Close New Todo' : 'New Todo'}
+          </button>
+          {showAddTodoForm && <NewTodoForm addTodo={addTodo} />}
+        </div>
+      </div>
     </div>
   );
 }
